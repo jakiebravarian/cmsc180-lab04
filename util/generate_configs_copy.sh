7ea7ba79724c0c80e2d5c8cf2d84be00371a880a
jakiebravarian <jcbrabante@up.edu.ph>
2025-05-22 23:58:43 +0800

    config to laptop > pc

--- util/generate_configs_copy.sh
+++ util/generate_configs_copy.sh
@@ -8,9 +8,7 @@ THREADS_LIST=(1 2 4 8 16)
 
 # Read IPs
 mapfile -t IPS < "$IP_FILE"
-PC_IP="${IPS[0]}"
-LAPTOP_IP="${IPS[1]}"
-MASTER_IP="$PC_IP"
+MASTER_IP="${IPS[0]}"
 
 # Create config directory if it doesn't exist
 mkdir -p "$CONFIG_DIR"
@@ -20,16 +18,12 @@ for t in "${THREADS_LIST[@]}"; do
     echo "Generating $CONFIG_FILE..."
 
     {
-        echo "${MASTER_IP}:${PORT_START}"  # Master port
-        echo "$t"                          # Number of threads
-
+        echo "${MASTER_IP}:${PORT_START}"
+        echo "$t"
         for ((i=1; i<=t; i++)); do
+            SLAVE_IP="${IPS[i]}"
             PORT=$((PORT_START + i))
-            if (( i <= t / 2 )); then
-                echo "${PC_IP}:${PORT}"
-            else
-                echo "${LAPTOP_IP}:${PORT}"
-            fi
+            echo "${SLAVE_IP}:${PORT}"
         done
     } > "$CONFIG_FILE"
 done
